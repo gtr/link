44f5910c55cf1823aea111021bbf14334874e1ff
Gerardo <gtc250@nyu.edu>
2018-08-06 14:30:13 -0400

    Add server.js

---
+++ server.js
@@ -0,0 +1,18 @@
+// server setup
+var express = require('express');
+var app = express();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+
+// on connection
+io.on('connection', function (socket){
+    console.log('user connected')
+    socket.on('disconnect', function () {
+        console.log('user disconnected')
+    });
+});
+
+// listen for requests on port 4000
+http.listen(4000, function () {
+    console.log('listening on port 3000');
+});
