b555855371105cccd54089627375a06f49498319
Gerardo <gtc250@nyu.edu>
2018-08-08 14:18:29 -0400

    Handle events

--- index.js
+++ index.js
@@ -1,10 +1,9 @@
 //app setup
 var io = require('socket.io-client');
-//var blessed = require('blessed');
+var blessed = require('blessed');
 
 // import blessed objects from ui directory
 var screen = require('./cli/screen.js');
-var loading = require('./cli/loading.js');
 var input = require('./cli/input.js');
 var body = require('./cli/body.js');
 
@@ -13,22 +12,52 @@ var socket = io('http://localhost:4000');
 
 // log function
 const log = (text) => {
+    //text = '{center}' + text + '{/center}';
     body.pushLine(text);
     screen.render();
 }
 
+// create a enterHandle box to identify client
+var enterHandle = blessed.box({
+    top: 'center',
+    left: 'center',
+    width: '50%',
+    height: '20%',
+    content: '{bold}Enter handle{/bold}!',
+    tags: true,
+    border: {
+        type: 'line'
+    },
+    style: {
+        fg: 'white',
+        bg: 'cyan',
+        border: {
+            fg: '#f0f0f0'
+        },
+        hover: {
+            bg: 'green'
+        }
+    }
+});
+
 // when connected...
 socket.on('connect', function() {
-    console.log('made socket connection')
     screen.title = 'ultra chat';
-
-    screen.append(loading);
+    // emit that new user has connected
+    socket.emit('newUser', {
+        caption: 'new user'
+    })
+    screen.append(enterHandle);
     screen.append(body);
     screen.append(input);
     // Handle submitting data
     input.on('submit', (text) => {
-        log(text);
+        //log(text);
         input.clearValue();
+        socket.emit('chat', {
+            //handle: handle,
+            message: text
+        })
     });
     screen.render();
     screen.key('enter', (ch, key) => {
@@ -36,7 +65,19 @@ socket.on('connect', function() {
     });
     input.focus();
     screen.render();
+    screen.render();
 });
+
+// on chat message...
+socket.on('chat', function(data){
+    log(data.message);
+});
+
+// on new user...
+socket.on('newUser', function(data) {
+    log(data.caption);
+})
+
 
 // Quit on Escape, q, or Control-C.
 screen.key(['escape', 'q', 'C-c'], function (ch, key) {
--- server.js
+++ server.js
@@ -4,15 +4,27 @@ var app = express();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 
-// on connection
-io.on('connection', function (socket){
+// on connection...
+io.on('connection', function(socket){
     console.log('user connected')
+    // on new user...
+    socket.on('newUser', function(data) {
+        io.sockets.emit('newUser', data);
+    })
+    // on disconnected user...
     socket.on('disconnect', function () {
         console.log('user disconnected')
     });
+    // on chat connection...
+    socket.on('chat', function(data) {
+        message = data.message;
+        handle = data.handle;
+        io.sockets.emit('chat', data);
+        console.log('message:', data.message)
+    })
 });
 
 // listen for requests on port 4000
-http.listen(4000, function () {
+http.listen(4000, function() {
     console.log('listening on port 4000');
 });
