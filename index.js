5b77c8c567b01e82847b2e8b1219da001e5a40e0
Gerardo <gtc250@nyu.edu>
2018-08-08 16:08:18 -0400

    Add handle mechanics

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 var io = require('socket.io-client');
 var blessed = require('blessed');
 
-// import blessed objects from ui directory
+// import blessed objects from cli directory
 var screen = require('./cli/screen.js');
 var input = require('./cli/input.js');
 var body = require('./cli/body.js');
@@ -12,74 +12,133 @@ var socket = io('http://localhost:4000');
 
 // log function
 const log = (text) => {
-    //text = '{center}' + text + '{/center}';
     body.pushLine(text);
     screen.render();
 }
 
-// create a enterHandle box to identify client
-var enterHandle = blessed.box({
-    top: 'center',
+// create a enterHandle form to identify client
+var enterHandle = blessed.form({
+    parent: screen,
+    width: '90%',
     left: 'center',
-    width: '50%',
-    height: '20%',
-    content: '{bold}Enter handle{/bold}!',
-    tags: true,
+    keys: true,
+    vi: true
+});
+
+var label1 = blessed.text({
+    parent: screen,
+    top: 0,
+    left: 'center',
+    content: 'FIRST NAME:',
+    style: {
+        fg: 'white'
+    }
+});
+
+// create handle textbox
+var handle = blessed.textbox({
+    parent: enterHandle,
+    name: 'handle',
+    top: 4,
+    left: 5,
+    height: 3,
+    inputOnFocus: true,
+    content: 'first',
     border: {
         type: 'line'
     },
+    focus: {
+        fg: 'blue'
+    }
+});
+
+// create submit button
+var enter = blessed.button({
+    parent: enterHandle,
+    name: 'submit',
+    content: 'Enter',
+    top: 10,
+    left: 5,
+    shrink: true,
+    padding: {
+        top: 1,
+        right: 2,
+        bottom: 1,
+        left: 2
+    },
     style: {
+        bold: true,
         fg: 'white',
-        bg: 'cyan',
-        border: {
-            fg: '#f0f0f0'
-        },
-        hover: {
-            bg: 'green'
+        bg: 'green',
+        focus: {
+            inverse: true
         }
     }
+});
+
+// display notification from server
+var notification = blessed.message({
+    parent: screen,
+    top: 40,
+    left: 5,
+    style: {
+        italic: true,
+        fg: 'green'
+    }
+});
+
+// handle events
+enter.on('press', function () {
+    enterHandle.submit();
+});
+
+enterHandle.on('submit', function (data) {
+    handle = data.handle;
+    socket.emit('submitHandle', {
+        handle: handle
+    });
+    socket.emit('enterChat', handle);
 });
 
 // when connected...
 socket.on('connect', function() {
     screen.title = 'ultra chat';
-    // emit that new user has connected
-    socket.emit('newUser', {
-        caption: 'new user'
-    })
+    screen.append(label1);
     screen.append(enterHandle);
+    screen.render();
+});
+
+socket.on('enterChat', function() {
     screen.append(body);
     screen.append(input);
     // Handle submitting data
-    input.on('submit', (text) => {
+    input.on('submit', function(text) {
         //log(text);
         input.clearValue();
         socket.emit('chat', {
-            //handle: handle,
+            handle: handle,
             message: text
         })
     });
     screen.render();
-    screen.key('enter', (ch, key) => {
+    screen.key('enter', function(ch, key) {
         input.focus();
     });
     input.focus();
-    screen.render();
-    screen.render();
 });
 
 // on chat message...
 socket.on('chat', function(data){
-    log(data.message);
+    log(data.handle, data.message);
 });
 
 // on new user...
 socket.on('newUser', function(data) {
     log(data.caption);
-})
+});
 
 
 // Quit on Escape, q, or Control-C.
-screen.key(['escape', 'q', 'C-c'], function (ch, key) {
+screen.key(['escape', 'q', 'C-c'], function(ch, key) {
     return process.exit(0);
 });
--- server.js
+++ server.js
@@ -4,12 +4,36 @@ var app = express();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 
+// array to store handles
+var handlesArray = ["poop"];
+
 // on connection...
 io.on('connection', function(socket){
     console.log('user connected')
+    // on submitted handle...
+    socket.on('submitHandle', function (data) {
+        handle = data.handle
+        var exists = false;
+        handlesArray.forEach(function(item) {
+            if (item == handle) {
+                exists = true
+            };
+        });
+        if (exists == false) {
+            handlesArray.push(handle)
+        } else {
+            console.log('user exists')
+        };
+        console.log('new user:', handle)
+    })
     // on new user...
     socket.on('newUser', function(data) {
         io.sockets.emit('newUser', data);
+    })
+    // on entering chat...
+    socket.on('enterChat', function (data) {
+        console.log('entering chat for', data)
+        io.sockets.emit('enterChat', data);
     })
     // on disconnected user...
     socket.on('disconnect', function () {
