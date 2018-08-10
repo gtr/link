41ab618154b6ec42dfc6cbdb9faabe19f6942166
Gerardo <gtc250@nyu.edu>
2018-08-10 19:08:28 -0400

    Update server

--- server.js
+++ server.js
@@ -5,14 +5,16 @@ var http = require('http').Server(app);
 var io = require('socket.io')(http);
 
 // array to store handles
-var handlesArray = ["poop"];
+var handlesArray = ["admin"];
 
 // on connection...
 io.on('connection', function(socket){
     console.log('user connected')
-    // on submitted handle...
+    
+    // on submitted handle
     socket.on('submitHandle', function (data) {
         handle = data.handle
+        id = data.id
         var exists = false;
         handlesArray.forEach(function(item) {
             if (item == handle) {
@@ -21,25 +23,33 @@ io.on('connection', function(socket){
         });
         if (exists == false) {
             handlesArray.push(handle)
+            io.sockets.connected[id].emit('enterChat', handle);
         } else {
             console.log('user exists')
+            io.sockets.connected[id].emit('failed', handle);
         };
         console.log('new user:', handle)
     })
-    // on new user...
+    
+    // on new user
     socket.on('newUser', function(data) {
         io.sockets.emit('newUser', data);
     })
-    // on entering chat...
+    
+    // on entering chat
     socket.on('enterChat', function (data) {
-        console.log('entering chat for', data)
-        io.sockets.emit('enterChat', data);
+        handle = data.handle
+        id = data.id
+        console.log('entering chat for', handle)
+        io.sockets.connected[id].emit('enterChat', handle);
     })
-    // on disconnected user...
+    
+    // on disconnected user
     socket.on('disconnect', function () {
-        console.log('user disconnected')
+        console.log('user disconnected', handle)
     });
-    // on chat connection...
+    
+    // on chat connection
     socket.on('chat', function(data) {
         message = data.message;
         handle = data.handle;
